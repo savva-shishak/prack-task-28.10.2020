5c778a5c4ba02a3ec262ae335e1a0e72ed47ccb2
savva-shishak <s.shishak@yandex.ru>
2020-10-28 21:49:17 +0500

    task-3

---
+++ script.js
@@ -0,0 +1,50 @@
+const form = document.forms.namedItem('registration');
+
+const btn = form.getElementsByClassName('form__btn')[0];
+const ind = form.getElementsByClassName('form__indicator')[0];
+const msg = form.getElementsByClassName('form__msg')[0];
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const values = getValuesAtForm()
+
+  hideBtnAndShowIndicator();
+
+  sendRequest({url: form.action, data: values})
+});
+
+function getValuesAtForm() {
+  const {value: surname}    = form.elements['surname'];
+  const {value: name}       = form.elements['name'];
+  const {value: patronymic} = form.elements['patronymic'];
+  const {value: phone}      = form.elements['phone'];
+  const {value: region}     = form.elements['region'];
+
+  return {surname, name, patronymic, phone, region};
+}
+
+function hideBtnAndShowIndicator() {
+  btn.style.display = 'none';
+  ind.classList.add('indicator_open');
+}
+
+function sendRequest({url, data}) {
+  fetch({url, body: data})
+    .then(at3000ms(showSucces))
+    .catch(at3000ms(showError));
+}
+
+function showSucces() {
+  ind.classList.remove('indicator_open');
+  msg.innerHTML = '<div class="msg msg_succes">Форма успешно отправлена!</div>'
+}
+
+function showError() {
+  ind.classList.remove('indicator_open');
+  msg.innerHTML = '<div class="msg msg_danger">Ошибка! <small>Скорее всего, потому что не правильный URL</small></div>';
+}
+
+function at3000ms(func) {
+  return () => setTimeout(func, 3000);
+}
